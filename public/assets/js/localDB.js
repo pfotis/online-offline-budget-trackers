159e8f475eb8fd4bf9d70f564a8d8ca471ac4db9
pfotis <69961508+pfotis@users.noreply.github.com>
2021-03-29 12:36:02 +1100

    complete the localDB.js file

--- public/assets/js/localDB.js
+++ public/assets/js/localDB.js
@@ -18,7 +18,36 @@ request.onerror = function(event) {
     console.log("error! " + event.target.errorCode);
 };
 
+function saveRecord(record) {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  store.add(record);
+}
 
+function checkDatabase() {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+      .then(response => response.json())
+      .then(() => {
+        const transaction = db.transaction(["pending"], "readwrite");
+        const store = transaction.objectStore("pending");
+        store.clear();
+      });
+    }
+  };
+}
 
 // listen for app coming back online
 window.addEventListener("online", checkDatabase);
